27c06a2d252364fe49fb9773a286bef99e16fca3
RecursiveZero <recursivezero@outlook.com>
2026-09-11 01:05:13 +0530

    100

--- frontend/src/components/ConnectionTab.tsx
+++ frontend/src/components/ConnectionTab.tsx
@@ -264,7 +264,7 @@ export default function ConnectionTab({
             </label>
 
             <label className="field field-full">
-              <span>Connection name 1.10</span>
+              <span>Connection name 1.100</span>
 
               <div className="field-with-action">
                 <input
